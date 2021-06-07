d595b1355e3eaba47fe843a34e63f8b465307741
Ahmed Shaikh <asofficial4k@gmail.com>
2021-06-07 15:42:58 +0500

    one step remain

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,8 @@
-const ErrorResponse = require('../utils/errorResponse');
+const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
+  console.log(error);
 
   error.message = err.message;
   error.name = err.name;
@@ -10,25 +11,30 @@ const errorHandler = (err, req, res, next) => {
   // console.log(error);
 
   // Mongoose ObjectID Error
-  if (error.name == 'CastError') {
+  if (error.name == "CastError") {
     const message = `Resource not found with the id of ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
 
   // Mongoose Duplicate Error
   if (error.code == 11000) {
-    const message = `Resource is already exist with the name of ${error.keyValue.name}`;
-    error = new ErrorResponse(message, 404);
+    const resourceWithSameName = `Resource is already exist with the name of ${error.keyValue.name}`;
+    const resourceWithSameEmail = `Resource is already exist with the email of ${error.keyValue.email}`;
+    if (error.keyValue.name) {
+      error = new ErrorResponse(resourceWithSameName, 404);
+    } else {
+      error = new ErrorResponse(resourceWithSameEmail, 404);
+    }
   }
 
   // Mongoose Empty Fields Error
-  if (error.name == 'ValidationError') {
-    const message = Object.values(error.errors).map(curr => curr.message);
+  if (error.name == "ValidationError") {
+    const message = Object.values(error.errors).map((curr) => curr.message);
     error = new ErrorResponse(message, 400);
   }
   res.status(error.statusCode || 500).json({
     success: false,
-    message: error.message || 'Server Error',
+    message: error.message || "Server Error",
   });
 };
 
